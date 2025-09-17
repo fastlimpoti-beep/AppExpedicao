a1a9a498f62465a8c8f01f28eaf9170cd752cf2f
LimpaForteTI <fastlimpo.ti@gmail.com>
2025-09-17 16:17:21 -0300

    Tentativa de quebra o teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,9 +14,9 @@ void main() {
     await tester.pumpWidget(const MainApp());
 
     // Exemplo: Verifica se existe um título específico ou widget principal
-    expect(find.text('Salve'), findsOneWidget);
+    expect(find.text(""), findsOneWidget);
 
     // Exemplo: Verifica se existe um botão esperado na tela inicial
-    expect(find.byType(ElevatedButton), findsWidgets);
+    expect(find.byType(TextButton), findsWidgets);
   });
 }
